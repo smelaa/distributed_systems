512e199d2332dc7ef96eb4e46fbc847f5d7d084c
Aleksandra Smela <aleksandra.smela@gmail.com>
2024-03-15 23:51:36 +0100

    task2 exception handling and async fetching

--- task2_rest/app.py
+++ task2_rest/app.py
@@ -4,8 +4,8 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from requests.exceptions import HTTPError, RequestException
 import requests
-
-API_NINJAS_KEY="962xfVTbMDGdZbShn3xtLg==xhJDmSDZpwimUfkL"
+import aiohttp
+from API_KEYS import API_NINJAS_KEY
 
 app=FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -26,6 +26,21 @@ def is_daytime(sunrise_sunset_data, current_time):
 
     return current_min>sunrise_min and current_min<sunset_min
 
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request: Request, exception: HTTPException):
+    status_code = exception.status_code
+    detail = exception.detail
+
+    return templates.TemplateResponse(
+        "error.html",
+        {
+            "request": request,
+            "status_code": status_code,
+            "detail": detail,
+        },
+        status_code=status_code,
+    )
+
 @app.get("/", response_class=HTMLResponse)
 async def homepage(request: Request):
     return templates.TemplateResponse("homepage.html", {"request": request})
@@ -40,26 +55,24 @@ async def post_results(request: Request, city: str = Form(None), current_time: s
     
     try:
         geocoding_response = requests.get(url="https://api.api-ninjas.com/v1/geocoding", params={"city": city}, headers={'X-Api-Key': API_NINJAS_KEY})
-        geocoding_response.raise_for_status()
         geocoding_response= geocoding_response.json()
 
-        if (len(geocoding_response)):
+        if (len(geocoding_response)==0):
             raise HTTPException(
-                status_code=500, detail=f"We cannot find city with the provided name!"
+                status_code=404, detail=f"We cannot find city with the provided name!"
             )
         
         geocoding_data = {"latitude":geocoding_response[0]["latitude"], "longitude":geocoding_response[0]["longitude"], "city":geocoding_response[0]["name"]}
-    except (KeyError, HTTPError, RequestException) as e:
+    except Exception as e:
         raise HTTPException(
             status_code=500, detail=f"Failed to fetch or parse GeocodingAPI data: {e}"
         )
     
     try:
         sunrise_sunset_response = requests.get(url="https://api.sunrise-sunset.org/json", params={"lat": geocoding_data["latitude"], "lng": geocoding_data["longitude"]})
-        sunrise_sunset_response.raise_for_status()
         sunrise_sunset_response= sunrise_sunset_response.json()
         sunrise_sunset_data = {"sunrise":sunrise_sunset_response["results"]["sunrise"], "sunset":sunrise_sunset_response["results"]["sunset"]}
-    except (KeyError, HTTPError, RequestException) as e:
+    except Exception as e:
         raise HTTPException(
             status_code=500, detail=f"Failed to fetch or parse Sunrise-sunset data: {e}"
         )
@@ -67,29 +80,40 @@ async def post_results(request: Request, city: str = Form(None), current_time: s
     to_print=""
     for_today=""
     daytime=""
+    task=""
 
     if is_daytime(sunrise_sunset_data, current_time):
-        try:
-            jokes_response = requests.get(url="https://api.api-ninjas.com/v1/jokes", params={"limit": 1}, headers={'X-Api-Key': API_NINJAS_KEY})
-            jokes_response.raise_for_status()
-            jokes_response= jokes_response.json()
-            to_print = jokes_response[0]["joke"]
-            for_today="Joke for today"
-            daytime="It's a daytime!"
-        except (KeyError, HTTPError, RequestException) as e:
-            raise HTTPException(
-                status_code=500, detail=f"Failed to fetch or parse Jokes data: {e}"
-            )
+        daytime="It's a daytime!"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url="https://api.api-ninjas.com/v1/jokes", params={"limit": 1}, headers={'X-Api-Key': API_NINJAS_KEY}) as jokes_response:
+                try:
+                    jokes_response= await jokes_response.json()
+                    to_print = jokes_response[0]["joke"]
+                    for_today="Joke for today"
+                except Exception as e:
+                    raise HTTPException(
+                        status_code=500, detail=f"Failed to fetch or parse Jokes data: {e}"
+                    )
+            
+            async with session.get(url="https://api.api-ninjas.com/v1/bucketlist", headers={'X-Api-Key': API_NINJAS_KEY}) as bucketlist_response:
+                try:
+                    bucketlist_response= await bucketlist_response.json()
+                    task = bucketlist_response["item"]
+                    for_today="Task for today"
+                except Exception as e:
+                    raise HTTPException(
+                        status_code=500, detail=f"Failed to fetch or parse Bucketlist data: {e}"
+                    )
     else:
+        daytime="It's a nightime!"
+        task="Go to sleep. Have a good night!"
         try:
             stoic_response = requests.get(url="https://stoic.tekloon.net/stoic-quote")
-            stoic_response.raise_for_status()
             stoic_response= stoic_response.json()
             print(stoic_response)
             to_print = stoic_response["quote"]+"\t~"+stoic_response["author"]
             for_today="Stoic quote for today"
-            daytime="It's a nightime!"
-        except (KeyError, HTTPError, RequestException) as e:
+        except Exception as e:
             raise HTTPException(
                 status_code=500, detail=f"Failed to fetch or parse Stoic quote data: {e}"
             )
@@ -101,13 +125,7 @@ async def post_results(request: Request, city: str = Form(None), current_time: s
         "city": geocoding_data["city"], 
         "sunrise": sunrise_sunset_data["sunrise"], 
         "sunset": sunrise_sunset_data["sunset"], 
-        "daytime": daytime
+        "daytime": daytime,
+        "task": task
         }
     return templates.TemplateResponse("results.html", context=data)
-
-
-
-
-    #porównaj czas
-    #zrequestuj odpowiednie api
-    #obsługa błędów itp
